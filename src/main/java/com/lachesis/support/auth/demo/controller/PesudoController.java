6e35c545e97980f7a543042f34a80a170ce93530
gavin2lee <gavin2lee@163.com>
2016-11-15 10:10:55 +0800

    demo

--- src/main/java/com/lachesis/support/auth/demo/controller/PesudoController.java
+++ src/main/java/com/lachesis/support/auth/demo/controller/PesudoController.java
@@ -1,11 +1,31 @@
 package com.lachesis.support.auth.demo.controller;
 
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lachesis.support.auth.demo.vo.Greeting;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/greetings")
 public class PesudoController {
+	private AtomicLong count = new AtomicLong();
 	
+	@RequestMapping("/greeting")
+	public Greeting getGreeting(@RequestParam(name="toname") String toName){
+		
+		return createNewGreeting(toName);
+	}
 	
+	private Greeting createNewGreeting(String counterparty){
+		Greeting g = new Greeting();
+		g.setId(String.valueOf(count.getAndIncrement()));
+		g.setGreeting(UUID.randomUUID().toString());
+		g.setCounterparty(counterparty);
+		
+		return g;
+	}
 }
---
+++ src/main/java/com/lachesis/support/auth/demo/vo/Greeting.java
@@ -0,0 +1,31 @@
+package com.lachesis.support.auth.demo.vo;
+
+public class Greeting {
+	private String id;
+	private String greeting;
+	private String counterparty;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCounterparty() {
+		return counterparty;
+	}
+
+	public void setCounterparty(String counterparty) {
+		this.counterparty = counterparty;
+	}
+}
