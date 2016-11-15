18935eba669835862b287aa337619c1bed020e6d
gavin2lee <gavin2lee@163.com>
2016-11-15 09:56:20 +0800

    init

---
+++ src/main/java/com/lachesis/support/auth/demo/controller/PesudoController.java
@@ -0,0 +1,11 @@
+package com.lachesis.support.auth.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PesudoController {
+	
+	
+}
