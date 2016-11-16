f1dbd714f92eb7874dab9a7a16e4b66617b0b687
gavin2lee <gavin2lee@163.com>
2016-11-16 10:07:31 +0800

    shiro

---
+++ src/test/java/com/lachesis/support/auth/shiro/LoginLogoutTest.java
@@ -0,0 +1,41 @@
+package com.lachesis.support.auth.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LoginLogoutTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		
+		try{
+			subject.login(token);
+		}catch(AuthenticationException ae){
+			ae.printStackTrace();
+		}
+		
+		Assert.assertEquals(true, subject.isAuthenticated());
+		
+		subject.logout();
+	}
+
+}
